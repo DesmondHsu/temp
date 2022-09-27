a8b99df6c5cae06311b765e08ac24c3dd8875cd1
Desmond Hsu <desmond.hsu@lendi.com.au>
2022-09-27 13:47:34 +1000

    calc

--- index.test.ts
+++ index.test.ts
@@ -1,7 +1,14 @@
-import {hello} from "./index";
+import {calculator} from "./index";
 
-describe('index', () => {
-    it('should greet', () => {
-        expect(hello('test')).toBe('hello test')
+describe('calculator', () => {
+    it('should calculate', () => {
+        const reactiveCalculator = calculator({a : 1, b: 2});
+
+        reactiveCalculator.updateA(2);
+        reactiveCalculator.updateB(3);
+        reactiveCalculator.updateC(7);
+        expect(reactiveCalculator.getA()).toBe(4);
+        expect(reactiveCalculator.getB()).toBe(3);
+        expect(reactiveCalculator.calculateC()).toBe(7);
     })
 })
--- index.ts
+++ index.ts
@@ -1,5 +1,23 @@
-export const hello = (a : string) => {
-    return `hello ${a}`;
-}
+export const calculator = ({a, b}) => {
 
-console.debug(hello('world'));
+    // Default behaviour
+    const calculateC = () => a + b;
+
+    // Update a
+    const updateA = (value) => a = value
+
+    // Update b
+    const updateB = (value) => b = value
+
+    // update c
+    const updateC = (value) => a = value - b
+
+    return {
+        getA: () => a,
+        getB: () => b,
+        calculateC,
+        updateA,
+        updateB,
+        updateC,
+    }
+}
